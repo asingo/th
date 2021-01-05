16132ed842fd37700f8d575662b642cc8bc12fed
Indra Hari Setiawan <indraharisetiawan@gmail.com>
2021-01-05 18:46:01 +0700

    update

--- assets/js/hapus.js
+++ assets/js/hapus.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
- 
+    
     $('#deleteModal').on('show.bs.modal', function (event) {
     var div = $(event.relatedTarget) // Tombol dimana modal di tampilkan
      
     // Untuk mengambil nilai dari data-id="" yang telah kita tempatkan pada link hapus
-
+    var title = div.data( "button-type" );
     var id = div.data('id')
     var modal = $(this)
-        modal.find('#modalDelete').attr("href","/?doDelete="+id);
+    //if(title == "user"){
+        modal.find('#modalDelete').attr("href","/?doDelete="+title);
+    //    }else if(title == "client"){
+    //    modal.find('#modalDelete').attr("href","/?doDeletsse="+id);
+    //    }
+        
     // Mengisi atribut href pada tombol ya yang kita berikan id hapus-true pada modal .
 })});
